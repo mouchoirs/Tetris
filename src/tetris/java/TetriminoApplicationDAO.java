d7aecce3602d45f08fd978df59ea28c98441f820
Unknown <c.boineau@live.fr>
2017-05-23 15:42:17 +0200

    hashmap

--- src/tetris/java/TetriminoApplicationDAO.java
+++ src/tetris/java/TetriminoApplicationDAO.java
@@ -1,10 +1,9 @@
 package tetris.java;
-import tetris.java.Tetrimino;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import com.sun.javafx.collections.MappingChange.Map;
 
 
 
