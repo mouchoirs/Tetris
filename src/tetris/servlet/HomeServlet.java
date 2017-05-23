f136cd38298549f8e47b66afc4493779521a3b8e
Unknown <criqui.sebastien@gmail.com>
2017-05-23 14:25:46 +0200

    ff

--- src/tetris/servlet/HomeServlet.java
+++ src/tetris/servlet/HomeServlet.java
@@ -1,6 +1,10 @@
 package tetris.servlet;
 
+
+
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +16,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/HomeServlet")
 public class HomeServlet extends HttpServlet {
+	  
+	     
+	  
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -28,7 +35,22 @@ public class HomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		 String login = request.getParameter("login");
+	     String pwd = request.getParameter("pwd");
+	     request.getSession().setAttribute("login", login);
+	     request.getSession().setAttribute("pwd", pwd);
+	        RequestDispatcher dispatcher = null;
+	         
+	         
+	        if(request.getParameter("login")!= null && request.getParameter("pwd")!= null){
+	            dispatcher = request.getRequestDispatcher("/WEB-INF/accueil.jsp");
+	        }else{
+	            dispatcher = request.getRequestDispatcher("/WEB-INF/login.jsp");
+	        }
+	        dispatcher.forward(request, response);
+	        }
+	         
+	    
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
