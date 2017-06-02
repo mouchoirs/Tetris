5257376edcb8aebda8e3949dbee62c884716058c
Unknown <criqui.sebastien@gmail.com>
2017-06-02 11:30:09 +0200

    block

--- src/src/fr/ascadis/model/Block.java
+++ src/src/fr/ascadis/model/Block.java
@@ -1,11 +1,15 @@
 package src.fr.ascadis.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Block  {
 
 	public Block() {
 		// TODO Auto-generated constructor stub
 		
-		
+	
+	List<Block> blocks =  new ArrayList<Block>() ;
 	}
 
 }
